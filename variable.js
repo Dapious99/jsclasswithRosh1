7b78aa9baf4c1a6032d7bad05faedabc7e819dd6
Dapious99 <dapious99@gmail.com>
2023-02-15 08:35:19 -0800

    first JS

---
+++ variable.js
@@ -0,0 +1,14 @@
+let firstNumber = 30
+let secondNumber = 35
+
+let sum = firstNumber+secondNumber
+let minus = firstNumber-secondNumber
+let divide = firstNumber/secondNumber
+let multiply = firstNumber*secondNumber
+let modulus = firstNumber%secondNumber
+
+console.log(sum)
+console.log(minus)
+console.log(divide)
+console.log(multiply)
+console.log(modulus)
